db32766a5da4390c15c8a4d6ea2916cb468138bd
Vasyl Hevko <hevko.vasyl.me@gmail.com>
2021-02-10 21:41:05 +0200

    123

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 import './styles.css';
+
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const startBtn = document.querySelector('button[data-action=start]');
+const stopBtn = document.querySelector('button[data-action=stop]');
+
+let timerIsActive = false;
+let timerId = null; 
+
+startBtn.addEventListener('click', () => {
+    if (timerIsActive) {
+        return
+    };
+    timerIsActive = true;
+    timerId = setInterval(() => {
+    let random = randomIntegerFromInterval(0, colors.length - 1);
+        document.body.style.backgroundColor = colors[random];
+    }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+    timerIsActive = false;
+});
+
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+
+
